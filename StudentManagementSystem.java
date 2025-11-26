bd8843ed0d663e074fb1ed20306415e79a3ad001
SHEHRRY999 <skbuisness999@gmail.com>
2025-11-26 19:43:02 +0500

    Update StudentManagementSystem.java

--- StudentManagementSystem.java
+++ StudentManagementSystem.java
@@ -2,6 +2,32 @@ import java.util.Scanner;
 public class StudentManagementSystem 
 {
   public static Scanner input = new Scanner(System.in);
+  public static void stdLogin()
+  {
+    System.out.println("----------> STUDENT LOGIN <----------");
+    System.out.println();
+    System.out.println();
+    System.out.println();
+    System.out.println("USERNAME : ");
+    String stdname = input.nextLine();
+    System.out.println();
+    System.out.println();
+    System.out.println("PASSWORD : ");
+    String stdpass = input.nextLine();
+  }
+  public static void admLogin()
+  {
+    System.out.println("----------> ADMIN LOGIN <----------");
+    System.out.println();
+    System.out.println();
+    System.out.println();
+    System.out.println("USERNAME : ");
+    String admname = input.nextLine();
+    System.out.println();
+    System.out.println();
+    System.out.println("PASSWORD : ");
+    String admpass = input.nextLine();
+  }
   public static void main(String[] args) 
   {
     System.out.println("----------> STUDENT MANAGEMENT SYSTEM <----------");
@@ -15,5 +41,24 @@ public class StudentManagementSystem
     System.out.println("--- 2. ADMIN LOGIN ---");
     System.out.println("--- 3. EXIT ---");
     System.out.println("Enter your choice ( 1 to 3 )");
+    int choice = input.nextInt();
+    switch(choice)
+      {
+        case 1:
+          stdLogin();
+        case 2:
+          admLogin();
+        case 3:
+          {
+            System.out.println("Exiting The Program");
+            return;
+          }
+        default:
+          {
+            System.out.println("Invalid Entry  Enter between 1 to 3");
+          }
+      }
+      
+    
   }
 }
