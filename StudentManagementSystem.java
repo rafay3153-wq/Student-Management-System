23dcbe6907449d6da9d18c2cfe53e71d9d89fd32
Muhammad Rafay <muhammadrafay3153@gmail.com>
2025-11-27 17:45:57 +0500

    Update StudentManagementSystem.java

--- StudentManagementSystem.java
+++ StudentManagementSystem.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 public class StudentManagementSystem 
 {
   public static Scanner input = new Scanner(System.in);
@@ -14,6 +14,8 @@ public class StudentManagementSystem
     System.out.println();
     System.out.println("PASSWORD : ");
     String stdpass = input.nextLine();
+    System.out.println("Login Successful!");
+    studentMenu();
   }
   public static void admLogin()
   {
@@ -27,6 +29,8 @@ public class StudentManagementSystem
     System.out.println();
     System.out.println("PASSWORD : ");
     String admpass = input.nextLine();
+    System.out.println("Admin Login Successful!");
+    adminMenu();
   }
   public static void main(String[] args) 
   {
@@ -40,7 +44,7 @@ public class StudentManagementSystem
     System.out.println("--- 1. STUDENT LOGIN ---");
     System.out.println("--- 2. ADMIN LOGIN ---");
     System.out.println("--- 3. EXIT ---");
-    System.out.println("Enter your choice ( 1 to 3 )");
+    System.out.println("Enter your choice: ");
     int choice = input.nextInt();
     switch(choice)
       {
@@ -58,7 +62,29 @@ public class StudentManagementSystem
             System.out.println("Invalid Entry  Enter between 1 to 3");
           }
       }
-      
-    
+      public static void adminMenu() {
+        while(true)
+          {
+             System.out.println("\n----------> ADMIN MENU <----------");
+            System.out.println("1. Add Student Record");
+            System.out.println("2. View All Student Records");
+            System.out.println("3. Update Student Details");
+            System.out.println("4. Search Student Records");
+            System.out.println("5. Save Records to File");
+            System.out.println("6. Logout");
+            System.out.print("Enter your choice: ");
+            int choice = input.nextInt();
+            switch(choice) {
+                case 1: addRecord(); break;
+                case 2: viewRecord(); break;
+                case 3: updateRecord(); break;
+                case 4: searchRecord(); break;
+                case 5: saveToFile(); break;
+                case 6: return;
+                default: System.out.println("Invalid Input!");
+              }            
+          }
+      }
+   
   }
 }
