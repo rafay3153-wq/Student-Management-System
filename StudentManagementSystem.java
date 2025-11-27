82ebef629857ae4875bf7073a105cc895cd55069
SHEHRRY999 <skbuisness999@gmail.com>
2025-11-27 18:30:49 +0500

    Update StudentManagementSystem.java

--- StudentManagementSystem.java
+++ StudentManagementSystem.java
@@ -4,7 +4,7 @@ public class StudentManagementSystem
   public static Scanner input = new Scanner(System.in);
   public static void stdLogin()
   {
-    System.out.println("----------> STUDENT LOGIN <----------");
+    System.out.println("\n----------> STUDENT LOGIN <----------");
     System.out.println();
     System.out.println();
     System.out.println();
@@ -14,12 +14,29 @@ public class StudentManagementSystem
     System.out.println();
     System.out.println("PASSWORD : ");
     String stdpass = input.nextLine();
-    System.out.println("Login Successful!");
-    studentMenu();
+    if( stdname.equals("FA25-BDS-045") && stdpass.equals("28076") )
+    {
+      System.out.println("Student Login Successful!");
+      System.out.println("Welcome Shehryar");
+      stdPanel();
+    }
+    else if( stdname.equals("FA25-BDS-035") && stdpass.equals("12345") )
+    {
+      System.out.println("Student Login Successful!");
+      System.out.println("Welcome Rafay");
+      stdPanel();
+    }
+    else
+    {
+      System.out.println("\nInvalid Username or Password");
+      System.out.println("\nReturning to Login menu");
+      return;
+    }
+      
   }
   public static void admLogin()
   {
-    System.out.println("----------> ADMIN LOGIN <----------");
+    System.out.println("\n----------> ADMIN LOGIN <----------");
     System.out.println();
     System.out.println();
     System.out.println();
@@ -32,59 +49,112 @@ public class StudentManagementSystem
     System.out.println("Admin Login Successful!");
     adminMenu();
   }
-  public static void main(String[] args) 
+  public static void stdPanel()
   {
-    System.out.println("----------> STUDENT MANAGEMENT SYSTEM <----------");
-    System.out.println("----------> SHEHRYAR KHURRAM FA25-BDS-045 <----------");
-    System.out.println("----------> MUHAMMAD RAFAY FA25-BDS-035 <----------");
-    System.out.println();
-    System.out.println();
-    System.out.println();
-    System.out.println("----------> LOGIN MENU <----------");
-    System.out.println("--- 1. STUDENT LOGIN ---");
-    System.out.println("--- 2. ADMIN LOGIN ---");
-    System.out.println("--- 3. EXIT ---");
-    System.out.println("Enter your choice: ");
-    int choice = input.nextInt();
-    switch(choice)
-      {
-        case 1:
-          stdLogin();
-        case 2:
-          admLogin();
-        case 3:
+    while(true)
           {
-            System.out.println("Exiting The Program");
-            return;
-          }
-        default:
-          {
-            System.out.println("Invalid Entry  Enter between 1 to 3");
-          }
-      }
-      public static void adminMenu() {
-        while(true)
-          {
-             System.out.println("\n----------> ADMIN MENU <----------");
-            System.out.println("1. Add Student Record");
-            System.out.println("2. View All Student Records");
-            System.out.println("3. Update Student Details");
-            System.out.println("4. Search Student Records");
-            System.out.println("5. Save Records to File");
-            System.out.println("6. Logout");
-            System.out.print("Enter your choice: ");
+            System.out.println("\n----------> STUDENT PANEL <----------");
+            System.out.println("--- 1. View Attendance ---");
+            System.out.println("--- 2. View Acadamics Report ---");
+            System.out.println("--- 3. View Profile ---");
+            System.out.println("--- 4. Log out ---");
+            System.out.print("Enter your choice ( 1 to 6 ) : ");
             int choice = input.nextInt();
-            switch(choice) {
-                case 1: addRecord(); break;
-                case 2: viewRecord(); break;
-                case 3: updateRecord(); break;
-                case 4: searchRecord(); break;
-                case 5: saveToFile(); break;
-                case 6: return;
-                default: System.out.println("Invalid Input!");
+            switch(choice) 
+            {
+                case 1: 
+                  stdAttendance(); 
+                  break;
+                case 2: 
+                  stdAcadamics(); 
+                  break;
+                case 3: 
+                  stdProfile(); 
+                  break;
+                case 4:
+                {
+                  System.out.println("Exiting The Program");
+                  return;
+                }
+                default: 
+                  System.out.println("Invalid Input!");
               }            
           }
       }
+  public static void adminPanel() 
+      {
+        while(true)
+          {
+            System.out.println("\n----------> ADMIN PANEL <----------");
+            System.out.println("--- 1. Add Student Record ---");
+            System.out.println("--- 2. View All Student Records ---");
+            System.out.println("--- 3. Update Student Details ---");
+            System.out.println("--- 4. Search Student Records ---");
+            System.out.println("--- 5. Save Records to File ---");
+            System.out.println("--- 6. Logout ---");
+            System.out.print("Enter your choice ( 1 to 6 ) : ");
+            int choice = input.nextInt();
+            switch(choice) 
+            {
+                case 1: 
+                  addRecord(); 
+                  break;
+                case 2: 
+                  viewRecord(); 
+                  break;
+                case 3: 
+                  updateRecord(); 
+                  break;
+                case 4: 
+                  searchRecord(); 
+                  break;
+                case 5: 
+                  saveToFile(); 
+                  break;
+                case 6: 
+                  return;
+                default: 
+                  System.out.println("Invalid Input!");
+              }            
+          }
+      }
+  public static void main(String[] args) 
+  {
+    while(true)
+      {
+        System.out.println("----------> STUDENT MANAGEMENT SYSTEM <----------");
+        System.out.println("----------> SHEHRYAR KHURRAM FA25-BDS-045 <----------");
+        System.out.println("----------> MUHAMMAD RAFAY FA25-BDS-035 <----------");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("----------> LOGIN MENU <----------");
+        System.out.println("--- 1. STUDENT LOGIN ---");
+        System.out.println("--- 2. ADMIN LOGIN ---");
+        System.out.println("--- 3. EXIT ---");
+        System.out.println("Enter your choice ( 1 to 3 ) : ");
+        int choice = input.nextInt();
+        switch(choice)
+        {
+          case 1:
+            stdLogin();
+            break;
+          case 2:
+            admLogin();
+            break;
+          case 3:
+            {
+              System.out.println("Exiting The Program");
+              return;
+            }
+          default:
+            System.out.println("Invalid Entry  Enter between 1 to 3");
+          
+        }
+      }
+    
+    
+      
    
   }
 }
